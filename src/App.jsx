0cbb9a15a6237db53582d74461c6dca0e1c01d8a
Aryan Nagbanshi <aryan.nagbanshi22@it.sce.edu.in>
2023-10-20 00:56:56 +0530

    Prettier Configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-
-  return (
-    <div>
-      <div className="navbar">
-
-      </div>
-      <div className="main">
-
-      </div>
-      <div className="footer">
-        
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<div className="navbar">Hello</div>
+			<div className="main"></div>
+			<div className="footer"></div>
+			<div>Hello</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
